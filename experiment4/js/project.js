eb62a53cdb5138b2e7ad5dcd2e1b7baa98c7d52d
Zosia Trela <ztrela@ucsc.edu>
2025-05-05 11:08:04 -0700

    experiment 4- changes to seeding and reflection

--- experiment4/js/project.js
+++ experiment4/js/project.js
@@ -1,235 +1,211 @@
-// project.js - Experiment 4 - Forest Stages
-// Author: Zosia Trela
-// Date: 4/29/25
+// // project.js - Experiment 4 - Forest Stages
+// // Author: Zosia Trela
+// // Date: 4/29/25
 
-let worldSeed;
+// //camping forest sketch in instance mode
+// var campingSketch = function(p) {
 
-// camping forest sketch in instance mode
-var campingSketch = function(p) {
+//   let smallDirt, mediumDirt, largeDirt, grass, water;
+//   let tree1, tree2, tree3, tree4, tree5, tree6;
+//   let tent;
+//   let grass1, grass2, flower2, flower3;
 
-  let smallDirt, mediumDirt, largeDirt, grass, water;
-  let tree1, tree2, tree3, tree4, tree5, tree6;
-  let tent;
-  let grass1, grass2, flower2, flower3;
+//   let tileType = {};
 
-  let tileType = {};
+//   // load tilesheet
+//   p.preload = function() {
+//     //ground tiles
+//     grass = p.loadImage('../img/grass.png');
+//     smallDirt = p.loadImage('../img/smallDirt.png');
+//     mediumDirt = p.loadImage('../img/mediumDirt.png');
+//     largeDirt = p.loadImage('../img/largeDirt.png');
+//     water = p.loadImage('../img/water.png');
+//     //tree assets
+//     tree1 = p.loadImage('../img/tree1.png');
+//     tree2 = p.loadImage('../img/tree2.png');
+//     tree3 = p.loadImage('../img/tree3.png');
+//     tree4 = p.loadImage('../img/tree4.png');
+//     tree5 = p.loadImage('../img/tree5.png');
+//     tree6 = p.loadImage('../img/tree6.png');
+//     //decor assets
+//     grass1 = p.loadImage('../img/grass1.png');
+//     grass2 = p.loadImage('../img/grass2.png');
+//     flower2 = p.loadImage('../img/flower2.png');
+//     flower3 = p.loadImage('../img/flower3.png');
 
-  // load tilesheet
-  p.preload = function() {
-    //ground tiles
-    grass = p.loadImage('../img/grass.png');
-    smallDirt = p.loadImage('../img/smallDirt.png');
-    mediumDirt = p.loadImage('../img/mediumDirt.png');
-    largeDirt = p.loadImage('../img/largeDirt.png');
-    water = p.loadImage('../img/water.png');
-    //tree assets
-    tree1 = p.loadImage('../img/tree1.png');
-    tree2 = p.loadImage('../img/tree2.png');
-    tree3 = p.loadImage('../img/tree3.png');
-    tree4 = p.loadImage('../img/tree4.png');
-    tree5 = p.loadImage('../img/tree5.png');
-    tree6 = p.loadImage('../img/tree6.png');
-    //decor assets
-    grass1 = p.loadImage('../img/grass1.png');
-    grass2 = p.loadImage('../img/grass2.png');
-    flower2 = p.loadImage('../img/flower2.png');
-    flower3 = p.loadImage('../img/flower3.png');
+//     tent = p.loadImage('../img/tent.png');
+//   };
 
-    tent = p.loadImage('../img/tent.png');
-  };
+//   p.setup = function(){
+//     tw = p3_tileWidth();
+//     th = p3_tileHeight();
+//   }
 
-  p.setup = function(){
-    tw = p3_tileWidth();
-    th = p3_tileHeight();
-  }
-
-  p.draw = function() {
-    p.background(220); // clear background
+//   p.draw = function() {
+ 
+//   };
   
-    p.translate(p.width / 2, p.height / 2); // center the canvas
-  
-    // draw 3 tiles manually to test rendering
-    p.push();
-    p.translate(0, 0);
-    p.drawTile(0, 0);
-    p.pop();
-  
-    p.push();
-    p.translate(tw, 0);
-    p.drawTile(1, 0);
-    p.pop();
-  
-    p.push();
-    p.translate(-tw, 0);
-    p.drawTile(-1, 0);
-    p.pop();
-  };
-  
-  
-  
+//   function p3_tileWidth() {
+//     return 32;
+//   }
+//   function p3_tileHeight() {
+//     return 16;
+//   }
 
-  function p3_tileWidth() {
-    return 32;
-  }
-  function p3_tileHeight() {
-    return 16;
-  }
-
-  p.worldKeyChanged = function(key){
-    worldSeed = XXH.h32(key, 0);
-    p.noiseSeed(worldSeed);
-    p.randomSeed(worldSeed);    
+//   p.worldKeyChanged = function(key){
+//     worldSeed = XXH.h32(key, 0);
+//     p.noiseSeed(worldSeed);
+//     p.randomSeed(worldSeed);    
     
-    //reseting tents
-    let clicks = {};
+//     //reseting tents
+//     let clicks = {};
   
-    // initialize tileType
-    tileType = {}; // clear previous
-    for (let i = -50; i <= 50; i++) {
-      for (let j = -50; j <= 50; j++) {
-        let n = p.noise(i * 0.1, j * 0.1);
-        let isWater = (n < 0.25);
-        tileType[[i, j]] = {isWater: isWater};
-      }
-    }
-  }
+//     // initialize tileType
+//     tileType = {}; // clear previous
+//     for (let i = -50; i <= 50; i++) {
+//       for (let j = -50; j <= 50; j++) {
+//         let n = p.noise(i * 0.1, j * 0.1);
+//         let isWater = (n < 0.25);
+//         tileType[[i, j]] = {isWater: isWater};
+//       }
+//     }
+//   }
 
-  let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
+//   let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
 
-  let clicks = {};
+//   let clicks = {};
 
-  p.tileClicked = function(i, j) {
-    // tile type for the current tile
-    let key = [i-1, j-1];
-    let tile = tileType[key];
-    let isWater = tile ? tile.isWater : (noise(i * 0.1, j * 0.1) < 0.25);
+//   p.tileClicked = function(i, j) {
+//     // tile type for the current tile
+//     let key = [i-1, j-1];
+//     let tile = tileType[key];
+//     let isWater = tile ? tile.isWater : (noise(i * 0.1, j * 0.1) < 0.25);
 
-    // place the tent if its not on water
-    if (!isWater) {
-      clicks[key] = 1 + (clicks[key] | 0);
-    }
-  }
+//     // place the tent if its not on water
+//     if (!isWater) {
+//       clicks[key] = 1 + (clicks[key] | 0);
+//     }
+//   }
 
-  p.drawTile = function(i, j) {
-    p.noStroke();
-    p.push();
-    p.imageMode(p.CENTER);
+//   p.drawTile = function(i, j) {
+//     p.noStroke();
+//     p.push();
+//     p.imageMode(p.CENTER);
 
-    // noise value for this tile
-    let n = p.noise(i * 0.1, j * 0.1);
+//     // noise value for this tile
+//     let n = p.noise(i * 0.1, j * 0.1);
 
-    // determine if this tile is water, and size
-    let isWater = (n < 0.25);
-    let isFlat = (n < 0.35);
-    let isSmall = (n < 0.55);
-    let isMedium = (n < 0.85);
-    let isLarge = (n < 1);
+//     // determine if this tile is water, and size
+//     let isWater = (n < 0.25);
+//     let isFlat = (n < 0.35);
+//     let isSmall = (n < 0.55);
+//     let isMedium = (n < 0.85);
+//     let isLarge = (n < 1);
     
-    tileType[[i, j]] = {isWater: isWater};
+//     tileType[[i, j]] = {isWater: isWater};
 
-    // draw ground tile
-    if (isWater) {
-      p.image(water, 0, 0, water.width/2, water.height/2);
-    } 
-    else if (isFlat) {
-      p.image(grass, 0, 0, grass.width/2, grass.height/2);
-    }
-    else if (isSmall) {
-      p.image(smallDirt, 0, 0, smallDirt.width/2, smallDirt.height/2);
-    }
-    else if (isMedium) {
-      p.image(mediumDirt, 0, 0, mediumDirt.width/2, mediumDirt.height/2);
-    }
-    else if (isLarge){
-      p.image(largeDirt, 0, 0, largeDirt.width/2, largeDirt.height/2);
-    }
+//     // draw ground tile
+//     if (isWater) {
+//       p.image(water, 0, 0, water.width/2, water.height/2);
+//     } 
+//     else if (isFlat) {
+//       p.image(grass, 0, 0, grass.width/2, grass.height/2);
+//     }
+//     else if (isSmall) {
+//       p.image(smallDirt, 0, 0, smallDirt.width/2, smallDirt.height/2);
+//     }
+//     else if (isMedium) {
+//       p.image(mediumDirt, 0, 0, mediumDirt.width/2, mediumDirt.height/2);
+//     }
+//     else if (isLarge){
+//       p.image(largeDirt, 0, 0, largeDirt.width/2, largeDirt.height/2);
+//     }
 
-    // draw trees not on water tiles
-    if (!isWater) {
-      let trees = [tree1, tree2, tree3, tree4, tree5, tree6, grass1, grass2, flower2, flower3,];
-      let treeChance = p.noise(i * 0.5, j * 0.5);
+//     // draw trees not on water tiles
+//     if (!isWater) {
+//       let trees = [tree1, tree2, tree3, tree4, tree5, tree6, grass1, grass2, flower2, flower3,];
+//       let treeChance = p.noise(i * 0.5, j * 0.5);
 
-      if (treeChance > 0.45) {
-        let treeIndex = XXH.h32("tree:" + [i, j], worldSeed) % trees.length;
-        let selectedTree = trees[treeIndex];
+//       if (treeChance > 0.45) {
+//         let treeIndex = XXH.h32("tree:" + [i, j], worldSeed) % trees.length;
+//         let selectedTree = trees[treeIndex];
 
-        let xOffset = map(p.noise(i * 0.7, j * 0.7), 0, 1, -10, 10);
-        let yOffset = map(p.noise(i * 0.9, j * 0.9), 0, 1, -10, 10);
+//         let xOffset = map(p.noise(i * 0.7, j * 0.7), 0, 1, -10, 10);
+//         let yOffset = map(p.noise(i * 0.9, j * 0.9), 0, 1, -10, 10);
 
-        //adjusting tree heights based on tile height
-        if (isSmall) {
-          yOffset += -5;
-        }
-        else if (isMedium) {
-          yOffset += -16;
-        }
-        else if (isLarge){
-          yOffset += -32;
-        }
+//         //adjusting tree heights based on tile height
+//         if (isSmall) {
+//           yOffset += -5;
+//         }
+//         else if (isMedium) {
+//           yOffset += -16;
+//         }
+//         else if (isLarge){
+//           yOffset += -32;
+//         }
 
-        p.image(selectedTree, xOffset, yOffset, selectedTree.width/2, selectedTree.height/2);
-      }
-    }
+//         p.image(selectedTree, xOffset, yOffset, selectedTree.width/2, selectedTree.height/2);
+//       }
+//     }
     
-    //adjusting tent heights based on tile height
-    let c = clicks[`${i},${j}`] ?? 0;
-    if (c % 2 === 1) {
-      let yOffset = 0;
+//     //adjusting tent heights based on tile height
+//     let c = clicks[`${i},${j}`] ?? 0;
+//     if (c % 2 === 1) {
+//       let yOffset = 0;
 
-      if (isFlat) {
-        yOffset = 0;
-      }
-      else if (isSmall) {
-        yOffset = -5;
-      }
-      else if (isMedium) {
-        yOffset = -16;
-      }
-      else if (isLarge){
-        yOffset = -32;
-      }
+//       if (isFlat) {
+//         yOffset = 0;
+//       }
+//       else if (isSmall) {
+//         yOffset = -5;
+//       }
+//       else if (isMedium) {
+//         yOffset = -16;
+//       }
+//       else if (isLarge){
+//         yOffset = -32;
+//       }
 
-      p.image(tent, 0, yOffset, tent.width/2, tent.height/2);
-    }
+//       p.image(tent, 0, yOffset, tent.width/2, tent.height/2);
+//     }
 
 
-    p.pop();
-  }
+//     p.pop();
+//   }
 
-  p.drawSelectedTile = function(i, j) {
-    push();
+//   p.drawSelectedTile = function(i, j) {
+//     push();
 
-    let tile = tileType[[i-1, j-1]];
-    let isWater;
-    if (tile !== undefined) {
-      isWater = tile.isWater;
-    } else {
-      let n = p.noise(i * 0.1, j * 0.1);
-      isWater = (n < 0.25);
-    }
+//     let tile = tileType[[i-1, j-1]];
+//     let isWater;
+//     if (tile !== undefined) {
+//       isWater = tile.isWater;
+//     } else {
+//       let n = p.noise(i * 0.1, j * 0.1);
+//       isWater = (n < 0.25);
+//     }
 
-    noFill();
-    if (isWater) {
-      stroke(255, 0, 0, 128);
-    } else {
-      stroke(0, 255, 0, 128);
-    }
+//     noFill();
+//     if (isWater) {
+//       stroke(255, 0, 0, 128);
+//     } else {
+//       stroke(0, 255, 0, 128);
+//     }
 
-    beginShape();
-    vertex(-tw, 0);
-    vertex(0, th);
-    vertex(tw, 0);
-    vertex(0, -th);
-    endShape(CLOSE);
+//     beginShape();
+//     vertex(-tw, 0);
+//     vertex(0, th);
+//     vertex(tw, 0);
+//     vertex(0, -th);
+//     endShape(CLOSE);
 
-    noStroke();
-    fill(100);
-    text("tile " + [i, j], 0, 0);
+//     noStroke();
+//     fill(100);
+//     text("tile " + [i, j], 0, 0);
 
-    p.pop();
-  }
+//     p.pop();
+//   }
 
-};
+// };
 
-// for html button
-var campingCanvas = new p5(campingSketch, 's1');
+// //for html button
--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,42 +1,179 @@
-//  sketch.js - Experiment 3 - Tile Dungeon and Overworld
+// sketch.js - Infinite Tile Dungeon and Overworld
 // Author: Zosia Trela
-// Date: 04/22/25
+// Date: 04/28/25
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+"use strict";
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+/* global XXH */
+/* exported -- 
+    p3_preload 
+    p3_setup 
+    p3_worldKeyChanged 
+    p3_tileWidth 
+    p3_tileHeight 
+    p3_tileClicked 
+    p3_drawBefore 
+    p3_drawTile 
+    p3_drawSelectedTile 
+    p3_drawAfter
+*/
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+let tw = 32;  // tile width
+let th = 16;  // tile height
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+let smallDirt, mediumDirt, largeDirt, grass, water;
+let tree1, tree2, tree3, tree4, tree5, tree6;
+let tent;
+let grass1, grass2, flower2, flower3;
 
-    myMethod() {
-        // code to run when method is called
-    }
+let tileType = {};
+let clicks = {};
+let worldSeed;
+let cameraPos = { x: 0, y: 0 };  // camera position to track player's view
+
+function p3_preload() {
+  // load ground tiles
+  grass = loadImage('../img/grass.png');
+  smallDirt = loadImage('../img/smallDirt.png');
+  mediumDirt = loadImage('../img/mediumDirt.png');
+  largeDirt = loadImage('../img/largeDirt.png');
+  water = loadImage('../img/water.png');
+  
+  // load tree assets
+  tree1 = loadImage('../img/tree1.png');
+  tree2 = loadImage('../img/tree2.png');
+  tree3 = loadImage('../img/tree3.png');
+  tree4 = loadImage('../img/tree4.png');
+  tree5 = loadImage('../img/tree5.png');
+  tree6 = loadImage('../img/tree6.png');
+  
+  // load decor assets
+  grass1 = loadImage('../img/grass1.png');
+  grass2 = loadImage('../img/grass2.png');
+  flower2 = loadImage('../img/flower2.png');
+  flower3 = loadImage('../img/flower3.png');
+  tent = loadImage('../img/tent.png');
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent('s1');
+
+  worldSeed = XXH.h32("default", 0);
+  noiseSeed(worldSeed);
+  randomSeed(worldSeed);
+
+  let button = document.getElementById("setSeedButton");
+  button.addEventListener("click", () => {
+    let val = document.getElementById("seedInput").value;
+    if (val.trim().length > 0) {
+      setSeed(val.trim());
+    }
+  });
+
+  // initialize the camera position
+  cameraPos = { x: 0, y: 0 };
 }
 
+function draw() {
+  background(200);
+  
+  let mouseXPos = mouseX - width / 2;
+  let mouseYPos = mouseY - height / 2;
 
+  // adjust camera position based on mouse position (or implement player movement logic)
+  cameraPos.x = mouseXPos;
+  cameraPos.y = mouseYPos;
 
-// mousePressed() function is called once after every time a mouse button is pressed
+  let viewRadiusX = Math.ceil(width / tw) + 2;  // calculate visible tiles on the X-axis
+  let viewRadiusY = Math.ceil(height / th) + 2; // calculate visible tiles on the Y-axis
+
+  translate(width / 2, height / 2);
+
+  // loop through tiles based on the camera position
+  for (let i = -viewRadiusX; i <= viewRadiusX; i++) {
+    for (let j = -viewRadiusY; j <= viewRadiusY; j++) {
+      push();
+      translate((i - j) * tw, (i + j) * th / 2);
+      drawTile(i, j);
+      pop();
+    }
+  }
+}
+
+function drawTile(i, j) {
+  noStroke();
+  imageMode(CENTER);
+
+  // generate terrain using noise
+  let n = noise(i * 0.1, j * 0.1);
+  let isWater = n < 0.25;
+  let isFlat = n < 0.35;
+  let isSmall = n < 0.55;
+  let isMedium = n < 0.85;
+  let isLarge = n < 1;
+
+  tileType[[i, j]] = { isWater: isWater };
+
+  if (isWater) {
+    image(water, 0, 0, water.width / 2, water.height / 2);
+  } else if (isFlat) {
+    image(grass, 0, 0, grass.width / 2, grass.height / 2);
+  } else if (isSmall) {
+    image(smallDirt, 0, 0, smallDirt.width / 2, smallDirt.height / 2);
+  } else if (isMedium) {
+    image(mediumDirt, 0, 0, mediumDirt.width / 2, mediumDirt.height / 2);
+  } else if (isLarge) {
+    image(largeDirt, 0, 0, largeDirt.width / 2, largeDirt.height / 2);
+  }
+
+  if (!isWater) {
+    let trees = [tree1, tree2, tree3, tree4, tree5, tree6, grass1, grass2, flower2, flower3];
+    let treeChance = noise(i * 0.5, j * 0.5);
+
+    if (treeChance > 0.45) {
+      let treeIndex = XXH.h32("tree:" + [i, j], worldSeed) % trees.length;
+      let selectedTree = trees[treeIndex];
+
+      let xOffset = map(noise(i * 0.7, j * 0.7), 0, 1, -10, 10);
+      let yOffset = map(noise(i * 0.9, j * 0.9), 0, 1, -10, 10);
+
+      if (isSmall) yOffset += -5;
+      else if (isMedium) yOffset += -16;
+      else if (isLarge) yOffset += -32;
+
+      image(selectedTree, xOffset, yOffset, selectedTree.width / 2, selectedTree.height / 2);
+    }
+  }
+
+  let c = clicks[`${i},${j}`] ?? 0;
+  if (c % 2 === 1) {
+    let yOffset = 0;
+    if (isSmall) yOffset = -5;
+    else if (isMedium) yOffset = -16;
+    else if (isLarge) yOffset = -32;
+    image(tent, 0, yOffset, tent.width / 2, tent.height / 2);
+  }
+}
+
 function mousePressed() {
-    // code to run when mouse is pressed
+  let x = mouseX - width / 2;
+  let y = mouseY - height / 2;
+
+  // loop through tiles near the mouse click position
+  for (let i = -50; i <= 50; i++) {
+    for (let j = -50; j <= 50; j++) {
+      let tx = (i - j) * tw;
+      let ty = (i + j) * th / 2;
+
+      if (dist(x, y, tx, ty) < tw / 2) {
+        let tile = tileType[[i, j]];
+        let isWater = tile ? tile.isWater : (noise(i * 0.1, j * 0.1) < 0.25);
+        if (!isWater) {
+          clicks[`${i},${j}`] = 1 + (clicks[`${i},${j}`] || 0);
+        }
+        return;
+      }
+    }
+  }
 }
